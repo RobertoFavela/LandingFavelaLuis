26386540c1e0092fdcb2708e9c20e1e8e495e16c
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2025-01-28 09:36:51 -0700

    As02 Mastermind

---
+++ As02/mastermind.kt
@@ -0,0 +1,32 @@
+
+/**
+ * Asignación 2: Mastermind
+ * Luis Roberto Favela Castro
+ * 00000246853
+ * 
+ */
+
+data class Evaluacion(val posicionExacta: Int, val posicionIncorrecta: Int)
+
+fun main(args: Array<String>) {
+    println(evaluar("ABCD", "ABCD") == Evaluacion(4, 0))
+    println(evaluar("ABCD", "CDBA") == Evaluacion(0, 4))
+    println(evaluar("ABCD", "ABDC") == Evaluacion(2, 2))
+    println(evaluar("AABC", "ADFE") == Evaluacion(1, 0))
+    println(evaluar("AABC", "DEAA") == Evaluacion(0, 2))
+}	
+
+fun evaluar(original: String, intento: String): Evaluacion {
+    var posicionesExactas = 0
+    var posicionesIncorrectas = 0
+
+    for (i in original.indices) {
+        if (original[i] == intento[i]) {
+            posicionesExactas++
+        } else if (intento[i] in original) {
+            posicionesIncorrectas++
+        }
+    }
+
+    return Evaluacion(posicionesExactas, posicionesIncorrectas)
+}
---
+++ Practicas/P1 - triangulos.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    fun tipoDeTriangulo(a: Int, b: Int, c: Int): String {
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return "numero menor a cero"
+        }
+
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return "triangulo invalido"
+        }
+
+        return when {
+            a == b && b == c -> "equilatero"
+            a == b || b == c || a == c -> "isosceles"
+            else -> "escaleno"
+        }
+    }
+}
